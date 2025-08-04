07698e8a4d89d4c1cd0a4fd9b05c8e767c840f90
Nastya Zenkovetc <lebed1412@yandex.ru>
2025-08-04 11:01:05 +0300

    edits

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,25 @@
 import image from "../img/goblin.png";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const cells = document.querySelectorAll(".cell");
+  const cells = [];
+  const container = document.querySelector(".container");
   const goblin = document.createElement("img");
   goblin.src = image;
-
   let currentCell = 0;
+
+  for(let i = 0; i < 16; i++) {
+    let cell = document.createElement("div");
+    container.append(cell);
+    cell.classList("cell");
+    cells.push(cell);
+  }
 
   function randomMovie() {
     cells[currentCell].innerHTML = "";
-    currentCell = Math.floor(Math.random() * cells.length);
+    let newCell;
+    do {
+      newCell = Math.floor(Math.random() * cells.length);
+    } while (newCell === currentCell);
     cells[currentCell].append(goblin);
   }
 
